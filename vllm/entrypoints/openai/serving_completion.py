ba48f10453a770848c250698b68282ce79fa1fd0
wanmok <chanwanmok@gmail.com>
2024-03-18 17:31:46 -0600

    Fixed echo support in OpenAI server.

--- vllm/entrypoints/openai/serving_completion.py
+++ vllm/entrypoints/openai/serving_completion.py
@@ -1,8 +1,15 @@
 import asyncio
 import time
 from fastapi import Request
-from typing import (AsyncGenerator, AsyncIterator, Callable, List, Optional,
-                    Dict, Tuple)
+from typing import (
+    AsyncGenerator,
+    AsyncIterator,
+    Callable,
+    List,
+    Optional,
+    Dict,
+    Tuple,
+)
 from vllm.logger import init_logger
 from vllm.utils import random_uuid
 from vllm.engine.async_llm_engine import AsyncLLMEngine
@@ -18,14 +25,16 @@ from vllm.entrypoints.openai.protocol import (
 from vllm.outputs import RequestOutput
 from vllm.entrypoints.openai.serving_engine import OpenAIServing, LoRA
 from vllm.model_executor.guided_decoding import (
-    get_guided_decoding_logits_processor)
+    get_guided_decoding_logits_processor,
+)
 
 logger = init_logger(__name__)
 
 TypeTokenIDs = List[int]
 TypeTopLogProbs = List[Optional[Dict[int, float]]]
 TypeCreateLogProbsFn = Callable[
-    [TypeTokenIDs, TypeTopLogProbs, Optional[int], int], LogProbs]
+    [TypeTokenIDs, TypeTopLogProbs, Optional[int], int], LogProbs
+]
 
 
 def parse_prompt_format(prompt) -> Tuple[bool, list]:
@@ -46,8 +55,10 @@ def parse_prompt_format(prompt) -> Tuple[bool, list]:
             prompt_is_tokens = True
             prompts = prompt  # case 4: array of token arrays
         else:
-            raise ValueError("prompt must be a string, array of strings, "
-                             "array of tokens, or array of token arrays")
+            raise ValueError(
+                "prompt must be a string, array of strings, "
+                "array of tokens, or array of token arrays"
+            )
     return prompt_is_tokens, prompts
 
 
@@ -87,17 +98,19 @@ def merge_async_iterators(*iterators):
 
 
 class OpenAIServingCompletion(OpenAIServing):
+    def __init__(
+        self,
+        engine: AsyncLLMEngine,
+        served_model: str,
+        lora_modules: Optional[List[LoRA]] = None,
+    ):
+        super().__init__(
+            engine=engine, served_model=served_model, lora_modules=lora_modules
+        )
 
-    def __init__(self,
-                 engine: AsyncLLMEngine,
-                 served_model: str,
-                 lora_modules: Optional[List[LoRA]] = None):
-        super().__init__(engine=engine,
-                         served_model=served_model,
-                         lora_modules=lora_modules)
-
-    async def create_completion(self, request: CompletionRequest,
-                                raw_request: Request):
+    async def create_completion(
+        self, request: CompletionRequest, raw_request: Request
+    ):
         """Completion API similar to OpenAI's API.
 
         See https://platform.openai.com/docs/api-reference/completions/create
@@ -114,7 +127,8 @@ class OpenAIServingCompletion(OpenAIServing):
         # Return error for unsupported features.
         if request.suffix is not None:
             return self.create_error_response(
-                "suffix is not currently supported")
+                "suffix is not currently supported"
+            )
 
         model_name = request.model
         request_id = f"cmpl-{random_uuid()}"
@@ -127,51 +141,64 @@ class OpenAIServingCompletion(OpenAIServing):
             lora_request = self._maybe_get_lora(request)
             guided_decode_logit_processor = (
                 await get_guided_decoding_logits_processor(
-                    request, await self.engine.get_tokenizer()))
+                    request, await self.engine.get_tokenizer()
+                )
+            )
             if guided_decode_logit_processor is not None:
                 if sampling_params.logits_processors is None:
                     sampling_params.logits_processors = []
                 sampling_params.logits_processors.append(
-                    guided_decode_logit_processor)
+                    guided_decode_logit_processor
+                )
             prompt_is_tokens, prompts = parse_prompt_format(request.prompt)
 
             for i, prompt in enumerate(prompts):
                 if prompt_is_tokens:
                     input_ids = self._validate_prompt_and_tokenize(
-                        request, prompt_ids=prompt)
+                        request, prompt_ids=prompt
+                    )
                 else:
                     input_ids = self._validate_prompt_and_tokenize(
-                        request, prompt=prompt)
+                        request, prompt=prompt
+                    )
 
                 generators.append(
-                    self.engine.generate(prompt,
-                                         sampling_params,
-                                         f"{request_id}-{i}",
-                                         prompt_token_ids=input_ids,
-                                         lora_request=lora_request))
+                    self.engine.generate(
+                        prompt if not prompt_is_tokens else None,
+                        sampling_params,
+                        f"{request_id}-{i}",
+                        prompt_token_ids=input_ids,
+                        lora_request=lora_request,
+                    )
+                )
         except ValueError as e:
             # TODO: Use a vllm-specific Validation Error
             return self.create_error_response(str(e))
 
-        result_generator: AsyncIterator[Tuple[
-            int, RequestOutput]] = merge_async_iterators(*generators)
+        result_generator: AsyncIterator[
+            Tuple[int, RequestOutput]
+        ] = merge_async_iterators(*generators)
 
         # Similar to the OpenAI API, when n != best_of, we do not stream the
         # results. In addition, we do not stream the results when use
         # beam search.
-        stream = (request.stream
-                  and (request.best_of is None or request.n == request.best_of)
-                  and not request.use_beam_search)
+        stream = (
+            request.stream
+            and (request.best_of is None or request.n == request.best_of)
+            and not request.use_beam_search
+        )
 
         # Streaming response
         if stream:
-            return self.completion_stream_generator(request,
-                                                    raw_request,
-                                                    result_generator,
-                                                    request_id,
-                                                    created_time,
-                                                    model_name,
-                                                    num_prompts=len(prompts))
+            return self.completion_stream_generator(
+                request,
+                raw_request,
+                result_generator,
+                request_id,
+                created_time,
+                model_name,
+                num_prompts=len(prompts),
+            )
 
         # Non-streaming response
         final_res_batch: RequestOutput = [None] * len(prompts)
@@ -183,7 +210,8 @@ class OpenAIServingCompletion(OpenAIServing):
                     return self.create_error_response("Client disconnected")
                 final_res_batch[i] = res
             response = self.request_output_to_completion_response(
-                final_res_batch, request, request_id, created_time, model_name)
+                final_res_batch, request, request_id, created_time, model_name
+            )
         except ValueError as e:
             # TODO: Use a vllm-specific Validation Error
             return self.create_error_response(str(e))
@@ -234,27 +262,37 @@ class OpenAIServingCompletion(OpenAIServing):
                         delta_token_ids = res.prompt_token_ids
                         top_logprobs = res.prompt_logprobs
                         has_echoed[i] = True
-                    elif (request.echo and request.max_tokens > 0
-                          and not has_echoed[i]):
+                    elif (
+                        request.echo
+                        and request.max_tokens > 0
+                        and not has_echoed[i]
+                    ):
                         # echo the prompt and first token
                         delta_text = res.prompt + output.text
-                        delta_token_ids = (res.prompt_token_ids +
-                                           output.token_ids)
-                        top_logprobs = res.prompt_logprobs + (output.logprobs
-                                                              or [])
+                        delta_token_ids = (
+                            res.prompt_token_ids + output.token_ids
+                        )
+                        top_logprobs = res.prompt_logprobs + (
+                            output.logprobs or []
+                        )
                         has_echoed[i] = True
                     else:
                         # return just the delta
-                        delta_text = output.text[len(previous_texts[i]):]
+                        delta_text = output.text[len(previous_texts[i]) :]
                         delta_token_ids = output.token_ids[
-                            previous_num_tokens[i]:]
-                        top_logprobs = output.logprobs[previous_num_tokens[
-                            i]:] if output.logprobs else None
+                            previous_num_tokens[i] :
+                        ]
+                        top_logprobs = (
+                            output.logprobs[previous_num_tokens[i] :]
+                            if output.logprobs
+                            else None
+                        )
 
                     if request.logprobs is not None:
                         assert top_logprobs is not None, (
                             "top_logprobs must be provided when logprobs "
-                            "is requested")
+                            "is requested"
+                        )
                         logprobs = self._create_logprobs(
                             token_ids=delta_token_ids,
                             top_logprobs=top_logprobs,
@@ -278,12 +316,14 @@ class OpenAIServingCompletion(OpenAIServing):
                                 logprobs=logprobs,
                                 finish_reason=finish_reason,
                             )
-                        ]).model_dump_json()
+                        ],
+                    ).model_dump_json()
                     yield f"data: {response_json}\n\n"
 
                     if output.finish_reason is not None:  # return final usage
-                        logprobs = LogProbs(
-                        ) if request.logprobs is not None else None
+                        logprobs = (
+                            LogProbs() if request.logprobs is not None else None
+                        )
                         prompt_tokens = len(res.prompt_token_ids)
                         completion_tokens = len(output.token_ids)
                         final_usage = UsageInfo(
@@ -328,6 +368,8 @@ class OpenAIServingCompletion(OpenAIServing):
             prompt_token_ids = final_res.prompt_token_ids
             prompt_logprobs = final_res.prompt_logprobs
             prompt_text = final_res.prompt
+            if prompt_text is None:
+                prompt_text = self.tokenizer.decode(prompt_token_ids)
 
             for output in final_res.outputs:
                 if request.echo and request.max_tokens == 0:
@@ -362,7 +404,8 @@ class OpenAIServingCompletion(OpenAIServing):
 
             num_prompt_tokens += len(prompt_token_ids)
             num_generated_tokens += sum(
-                len(output.token_ids) for output in final_res.outputs)
+                len(output.token_ids) for output in final_res.outputs
+            )
 
         usage = UsageInfo(
             prompt_tokens=num_prompt_tokens,
--- vllm/entrypoints/openai/serving_engine.py
+++ vllm/entrypoints/openai/serving_engine.py
@@ -6,11 +6,15 @@ from typing import Dict, List, Optional, Union
 from vllm.logger import init_logger
 from vllm.transformers_utils.tokenizer import get_tokenizer
 from vllm.engine.async_llm_engine import AsyncLLMEngine
-from vllm.entrypoints.openai.protocol import (CompletionRequest,
-                                              ChatCompletionRequest,
-                                              ErrorResponse, LogProbs,
-                                              ModelCard, ModelList,
-                                              ModelPermission)
+from vllm.entrypoints.openai.protocol import (
+    CompletionRequest,
+    ChatCompletionRequest,
+    ErrorResponse,
+    LogProbs,
+    ModelCard,
+    ModelList,
+    ModelPermission,
+)
 from vllm.lora.request import LoRARequest
 from vllm.sequence import Logprob
 
@@ -24,11 +28,12 @@ class LoRA:
 
 
 class OpenAIServing:
-
-    def __init__(self,
-                 engine: AsyncLLMEngine,
-                 served_model: str,
-                 lora_modules=Optional[List[LoRA]]):
+    def __init__(
+        self,
+        engine: AsyncLLMEngine,
+        served_model: str,
+        lora_modules=Optional[List[LoRA]],
+    ):
         self.engine = engine
         self.served_model = served_model
         if lora_modules is None:
@@ -39,7 +44,8 @@ class OpenAIServing:
                     lora_name=lora.name,
                     lora_int_id=i,
                     lora_local_path=lora.local_path,
-                ) for i, lora in enumerate(lora_modules, start=1)
+                )
+                for i, lora in enumerate(lora_modules, start=1)
             ]
 
         self.max_model_len = 0
@@ -66,19 +72,24 @@ class OpenAIServing:
         self.tokenizer = get_tokenizer(
             engine_model_config.tokenizer,
             tokenizer_mode=engine_model_config.tokenizer_mode,
-            trust_remote_code=engine_model_config.trust_remote_code)
+            trust_remote_code=engine_model_config.trust_remote_code,
+        )
 
     async def show_available_models(self) -> ModelList:
         """Show available models. Right now we only have one model."""
         model_cards = [
-            ModelCard(id=self.served_model,
-                      root=self.served_model,
-                      permission=[ModelPermission()])
+            ModelCard(
+                id=self.served_model,
+                root=self.served_model,
+                permission=[ModelPermission()],
+            )
         ]
         lora_cards = [
-            ModelCard(id=lora.lora_name,
-                      root=self.served_model,
-                      permission=[ModelPermission()])
+            ModelCard(
+                id=lora.lora_name,
+                root=self.served_model,
+                permission=[ModelPermission()],
+            )
             for lora in self.lora_requests
         ]
         model_cards.extend(lora_cards)
@@ -100,45 +111,54 @@ class OpenAIServing:
             step_top_logprobs = top_logprobs[i]
             if step_top_logprobs is not None:
                 token_logprob = step_top_logprobs[token_id].logprob
+                token = step_top_logprobs[token_id].decoded_token
             else:
                 token_logprob = None
-            token = step_top_logprobs[token_id].decoded_token
+                token = self.tokenizer.decode([token_id])
             logprobs.tokens.append(token)
             logprobs.token_logprobs.append(token_logprob)
             if len(logprobs.text_offset) == 0:
                 logprobs.text_offset.append(initial_text_offset)
             else:
-                logprobs.text_offset.append(logprobs.text_offset[-1] +
-                                            last_token_len)
+                logprobs.text_offset.append(
+                    logprobs.text_offset[-1] + last_token_len
+                )
             last_token_len = len(token)
 
             if num_output_top_logprobs:
-                logprobs.top_logprobs.append({
-                    p.decoded_token: p.logprob
-                    for i, p in step_top_logprobs.items()
-                } if step_top_logprobs else None)
+                logprobs.top_logprobs.append(
+                    {
+                        p.decoded_token: p.logprob
+                        for i, p in step_top_logprobs.items()
+                    }
+                    if step_top_logprobs
+                    else None
+                )
         return logprobs
 
     def create_error_response(
-            self,
-            message: str,
-            err_type: str = "BadRequestError",
-            status_code: HTTPStatus = HTTPStatus.BAD_REQUEST) -> ErrorResponse:
-        return ErrorResponse(message=message,
-                             type=err_type,
-                             code=status_code.value)
+        self,
+        message: str,
+        err_type: str = "BadRequestError",
+        status_code: HTTPStatus = HTTPStatus.BAD_REQUEST,
+    ) -> ErrorResponse:
+        return ErrorResponse(
+            message=message, type=err_type, code=status_code.value
+        )
 
     def create_streaming_error_response(
-            self,
-            message: str,
-            err_type: str = "BadRequestError",
-            status_code: HTTPStatus = HTTPStatus.BAD_REQUEST) -> str:
-        json_str = json.dumps({
-            "error":
-            self.create_error_response(message=message,
-                                       err_type=err_type,
-                                       status_code=status_code).model_dump()
-        })
+        self,
+        message: str,
+        err_type: str = "BadRequestError",
+        status_code: HTTPStatus = HTTPStatus.BAD_REQUEST,
+    ) -> str:
+        json_str = json.dumps(
+            {
+                "error": self.create_error_response(
+                    message=message, err_type=err_type, status_code=status_code
+                ).model_dump()
+            }
+        )
         return json_str
 
     async def _check_model(self, request) -> Optional[ErrorResponse]:
@@ -149,7 +169,8 @@ class OpenAIServing:
         return self.create_error_response(
             message=f"The model `{request.model}` does not exist.",
             err_type="NotFoundError",
-            status_code=HTTPStatus.NOT_FOUND)
+            status_code=HTTPStatus.NOT_FOUND,
+        )
 
     def _maybe_get_lora(self, request) -> Optional[LoRARequest]:
         if request.model == self.served_model:
@@ -161,18 +182,23 @@ class OpenAIServing:
         raise ValueError("The model `{request.model}` does not exist.")
 
     def _validate_prompt_and_tokenize(
-            self,
-            request: Union[ChatCompletionRequest, CompletionRequest],
-            prompt: Optional[str] = None,
-            prompt_ids: Optional[List[int]] = None) -> List[int]:
+        self,
+        request: Union[ChatCompletionRequest, CompletionRequest],
+        prompt: Optional[str] = None,
+        prompt_ids: Optional[List[int]] = None,
+    ) -> List[int]:
         if not (prompt or prompt_ids):
             raise ValueError("Either prompt or prompt_ids should be provided.")
-        if (prompt and prompt_ids):
+        if prompt and prompt_ids:
             raise ValueError(
-                "Only one of prompt or prompt_ids should be provided.")
+                "Only one of prompt or prompt_ids should be provided."
+            )
 
-        input_ids = prompt_ids if prompt_ids is not None else self.tokenizer(
-            prompt).input_ids
+        input_ids = (
+            prompt_ids
+            if prompt_ids is not None
+            else self.tokenizer(prompt).input_ids
+        )
         token_num = len(input_ids)
 
         if request.max_tokens is None:
@@ -185,6 +211,7 @@ class OpenAIServing:
                 f"{request.max_tokens + token_num} tokens "
                 f"({token_num} in the messages, "
                 f"{request.max_tokens} in the completion). "
-                f"Please reduce the length of the messages or completion.", )
+                f"Please reduce the length of the messages or completion.",
+            )
         else:
             return input_ids
